59f3de291fdd30657a3429cc9be0824c2d257042
Axenior <holisyogi399@gmail.com>
2024-11-15 17:38:29 +0700

    yes

--- belajar_Angular/src/app/housing-location/housing-location.component.ts
+++ belajar_Angular/src/app/housing-location/housing-location.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
@@ -11,6 +12,7 @@ import { HousingLocation } from '../housing-location';
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a routerlink="details">Show More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css'
